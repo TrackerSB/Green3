0833222861a39ef4296f2527613d58a14c6d68ab
Stefan Huber <stefan.huber.niedling@outlook.com>
2021-08-10 12:55:43 +0200

    Add empty JavaFX application

---
+++ Green3/src/main/java/bayern/steinbrecher/green3/Main.java
@@ -0,0 +1,17 @@
+package bayern.steinbrecher.green3;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import lombok.NonNull;
+
+/**
+ * @author Stefan Huber
+ * @since 3u00
+ */
+public final class Main extends Application {
+
+    @Override
+    public void start(@NonNull Stage primaryStage) {
+        primaryStage.show();
+    }
+}
---
+++ Green3/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module bayern.steinbrecher.Green3 {
+    requires javafx.graphics;
+
+    requires static lombok;
+
+    exports bayern.steinbrecher.green3 to javafx.graphics;
+}
