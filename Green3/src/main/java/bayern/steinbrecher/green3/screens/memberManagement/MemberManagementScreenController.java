80336bd6a27283c16b69eef11f87fc4aea3ebf2c
Stefan Huber <stefan.huber.niedling@outlook.com>
2021-09-07 13:41:16 +0200

    Clarify structure of asynchronous calls

--- Green3/src/main/java/bayern/steinbrecher/green3/screens/memberManagement/MemberManagementScreenController.java
+++ Green3/src/main/java/bayern/steinbrecher/green3/screens/memberManagement/MemberManagementScreenController.java
@@ -14,6 +14,7 @@ import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import lombok.NonNull;
 
 import java.net.UnknownHostException;
 import java.nio.charset.StandardCharsets;
@@ -34,44 +35,48 @@ public class MemberManagementScreenController extends ScreenController {
     @FXML
     private TableView<Membership> memberView;
 
+    private DBConnection connectToMemberDB() {
+        try {
+            return new SshConnection(
+                    SupportedDBMS.MY_SQL,
+                    "",
+                    3306,
+                    "",
+                    "",
+                    22,
+                    StandardCharsets.UTF_8,
+                    new SshCredentials(
+                            "",
+                            "",
+                            "",
+                            ""
+                    )
+            );
+        } catch (ConnectionFailedException | AuthException | UnknownHostException ex) {
+            throw new CompletionException("Could not request member data", ex);
+        }
+    }
+
+    private void setupMemberViewColumns(@NonNull DBConnection dbConnection) {
+        try {
+            @SuppressWarnings("Convert2MethodRef")
+            Collection<TableColumn<Membership, ?>> columns = dbConnection.getAllColumns(Tables.MEMBERS)
+                    .stream()
+                    .sorted(Comparator.comparingInt(DBConnection.Column::getIndex))
+                    .map(DBConnection.Column::getName)
+                    .map(n -> new TableColumn<Membership, String>(n))
+                    .collect(Collectors.toList());
+            Platform.runLater(() -> memberView.getColumns().setAll(columns));
+        } catch (QueryFailedException ex) {
+            throw new CompletionException("Could not set columns to member view", ex);
+        }
+    }
+
     @Override
     protected void afterScreenManagerIsSet() {
         CompletableFuture.runAsync(() -> getScreenManager().showOverlay("Retrieving data..."))
-                .thenApply(v -> {
-                    try {
-                        return new SshConnection(
-                                SupportedDBMS.MY_SQL,
-                                "",
-                                3306,
-                                "",
-                                "",
-                                22,
-                                StandardCharsets.UTF_8,
-                                new SshCredentials(
-                                        "",
-                                        "",
-                                        "",
-                                        ""
-                                )
-                        );
-                    } catch (ConnectionFailedException | AuthException | UnknownHostException ex) {
-                        throw new CompletionException("Could not request member data", ex);
-                    }
-                })
-                .thenAccept(dbConnection -> {
-                    try {
-                        @SuppressWarnings("Convert2MethodRef")
-                        Collection<TableColumn<Membership, ?>> columns = dbConnection.getAllColumns(Tables.MEMBERS)
-                                .stream()
-                                .sorted(Comparator.comparingInt(DBConnection.Column::getIndex))
-                                .map(DBConnection.Column::getName)
-                                .map(n -> new TableColumn<Membership, String>(n))
-                                .collect(Collectors.toList());
-                        Platform.runLater(() -> memberView.getColumns().setAll(columns));
-                    } catch (QueryFailedException ex) {
-                        throw new CompletionException("Could not set columns to member view", ex);
-                    }
-                })
+                .thenApply(v -> connectToMemberDB())
+                .thenAccept(this::setupMemberViewColumns)
                 .handle((result, throwable) -> {
                     if (throwable != null) {
                         LOGGER.log(Level.SEVERE, "Could not set up member view", throwable);
